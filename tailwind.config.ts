3f4c23cded20f0fded8109b38b88abe186cedb8a
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-04-28 18:31:45 +0400

    Added NextUI framework

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
+const { nextui } = require("@nextui-org/react");
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +17,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [ nextui() ],
 };
+
 export default config;

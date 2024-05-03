b19732090f923cf3eb32b9a87f77d9ec83f4b9ec
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-03 15:11:09 +0400

    Add detection of browser's language and redirects to the proper locale page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+"use client";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function RootPage() {
-	return redirect("/en");
+	const [componentMounted, setComponentMounted] = useState<boolean>(false);
+	const [languageCode, setLanguageCode] = useState<string>("en");
+
+	useEffect(() => {
+		setLanguageCode(window.navigator.language);
+		setComponentMounted(true);
+	}, []);
+
+	if (!componentMounted) {
+		return null;
+	}
+
+	return redirect(`/${languageCode}`);
 }

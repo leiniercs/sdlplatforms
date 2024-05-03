73fb962b2c2193fd1500db53cafa19e90e5da402
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-03 17:28:19 +0400

    Minor fixes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,6 @@
-import { ReactNode } from "react";
 import Header from "@/components/header";
 
-type CustomPageProps = {
-	children: ReactNode;
-	params: { locale: string };
-};
-
-export default function LocalePage({
-	children,
-	params: { locale }
-}: Readonly<CustomPageProps>) {
+export default function LocalePage() {
 	return (
 		<>
 			<header>

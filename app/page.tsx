9cb45406d690dcd3750b53f5bc242864ae5883ba
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-03 17:34:20 +0400

    Handle the language in full format

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,13 @@ export default function RootPage() {
 	const [languageCode, setLanguageCode] = useState<string>("en");
 
 	useEffect(() => {
-		setLanguageCode(window.navigator.language);
+		let language = window.navigator.language;
+
+		if (language.length > 2) {
+			language = language.substring(0, 2);
+		}
+
+		setLanguageCode(language);
 		setComponentMounted(true);
 	}, []);
 

4bbe78d68748f41f57a549f405818761afcb552c
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-05 12:21:40 +0400

    - Implemented server-side messages internationalization - Changed items font

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,8 @@
 "use client";
 import type { NavMenuItem } from "@/types/menu";
+import type { AbstractIntlMessages } from "next-intl";
 import { useState } from "react";
-import { useTranslations } from "next-intl";
 import {
-	Button,
 	Image,
 	Link as UILink,
 	Navbar,
@@ -14,22 +13,37 @@ import {
 	NavbarMenu,
 	NavbarMenuItem
 } from "@nextui-org/react";
+import { Rubik } from "next/font/google";
 import { Link, usePathname } from "@/components/navigation";
 
-export default function Header() {
+type CustomComponentsProps = {
+	messages: string | AbstractIntlMessages;
+};
+
+const itemFont = Rubik({ subsets: ["latin"], weight: "500" });
+
+export default function Header({ messages }: Readonly<CustomComponentsProps>) {
 	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-	const t = useTranslations("header");
 	const currentPathname = usePathname();
+
 	const menuItems: NavMenuItem[] = [
-		{ name: t("menuitems.home"), href: "/" },
-		{ name: t("menuitems.projects"), href: "/projects" }
+		// @ts-expect-error
+		{ name: messages.menuitems.home, href: "/" },
+		// @ts-expect-error
+		{ name: messages.menuitems.projects, href: "/projects" }
 	];
 
 	return (
-		<Navbar isBlurred isBordered onMenuOpenChange={setIsMenuOpen}>
+		<Navbar
+			classNames={{ item: itemFont.className }}
+			isBlurred
+			isBordered
+			onMenuOpenChange={setIsMenuOpen}
+		>
 			<NavbarContent justify="start">
 				<NavbarMenuToggle
-					aria-label={isMenuOpen ? t("closeMenu") : t("openMenu")}
+					// @ts-expect-error
+					aria-label={isMenuOpen ? messages.closeMenu : messages.openMenu}
 					className="sm:hidden"
 				/>
 				<NavbarBrand className="hidden md:inline-block">
@@ -37,41 +51,45 @@ export default function Header() {
 						<Image
 							classNames={{ img: "!max-h-[50px] !rounded-none" }}
 							src="/images/logo/logo-nobackground.svg"
-							alt={t("logo")}
+							// @ts-expect-error
+							alt={messages.logo}
 						/>
 					</Link>
 				</NavbarBrand>
 			</NavbarContent>
 
-			<NavbarContent className="flex sm:hidden" justify="center">
-				<NavbarBrand>
+			<NavbarContent justify="center">
+				<NavbarBrand className="flex sm:hidden">
 					<Link href="/">
 						<Image
 							classNames={{ img: "!max-h-[50px] !rounded-none" }}
 							src="/images/logo/logo-nobackground.svg"
-							alt={t("logo")}
+							// @ts-expect-error
+							alt={messages.logo}
 						/>
 					</Link>
 				</NavbarBrand>
 			</NavbarContent>
 
-			<NavbarContent className="hidden sm:flex gap-4" justify="end">
-				{menuItems.map((item: NavMenuItem, index: number) => {
-					const isActive: boolean = currentPathname === item.href;
+			<NavbarContent justify="end">
+				<div className="hidden sm:flex gap-4">
+					{menuItems.map((item: NavMenuItem, index: number) => {
+						const isActive: boolean = currentPathname === item.href;
 
-					return (
-						<NavbarItem key={index} isActive={isActive}>
-							<UILink
-								as={Link}
-								href={item.href}
-								color={isActive ? "primary" : "foreground"}
-								aria-current={isActive ? "page" : undefined}
-							>
-								{item.name}
-							</UILink>
-						</NavbarItem>
-					);
-				})}
+						return (
+							<NavbarItem key={index} isActive={isActive}>
+								<UILink
+									as={Link}
+									href={item.href}
+									color={isActive ? "primary" : "foreground"}
+									aria-current={isActive ? "page" : undefined}
+								>
+									{item.name}
+								</UILink>
+							</NavbarItem>
+						);
+					})}
+				</div>
 			</NavbarContent>
 
 			<NavbarMenu>

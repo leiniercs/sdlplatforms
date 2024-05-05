7d7db5958b9db1a6982093e416cbad299fcb3b7a
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-05 12:21:55 +0400

    Implemented server-side messages internationalization

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,32 +1,13 @@
 "use client";
-import type { AbstractIntlMessages } from "next-intl";
 import { ReactNode } from "react";
-import { useRouter } from "next/navigation";
-import { NextIntlClientProvider } from "next-intl";
 import { NextUIProvider } from "@nextui-org/react";
 
-type CustomProps = {
+type CustomComponentProps = {
 	children: ReactNode;
-	locale: string;
-	messages: AbstractIntlMessages;
 };
 
 export default function Providers({
-	children,
-	locale,
-	messages
-}: Readonly<CustomProps>) {
-	const router = useRouter();
-
-	return (
-		<NextUIProvider navigate={router.push}>
-			<NextIntlClientProvider
-				locale={locale}
-				messages={messages}
-				timeZone="UTC"
-			>
-				{children}
-			</NextIntlClientProvider>
-		</NextUIProvider>
-	);
+	children
+}: Readonly<CustomComponentProps>) {
+	return <NextUIProvider>{children}</NextUIProvider>;
 }

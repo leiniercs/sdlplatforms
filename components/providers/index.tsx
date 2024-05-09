80f1a18deccc5baed53bbbca34a895894b661d37
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-09 18:10:02 +0400

    Implemented client-side internationalization

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,13 +1,27 @@
 "use client";
+import type { AbstractIntlMessages } from "next-intl";
 import { ReactNode } from "react";
+import { NextIntlClientProvider } from "next-intl";
 import { NextUIProvider } from "@nextui-org/react";
 
 type CustomComponentProps = {
+	locale: string;
+	messages: AbstractIntlMessages;
 	children: ReactNode;
 };
 
 export default function Providers({
+	locale,
+	messages,
 	children
 }: Readonly<CustomComponentProps>) {
-	return <NextUIProvider>{children}</NextUIProvider>;
+	return (
+		<NextIntlClientProvider
+			locale={locale}
+			messages={messages}
+			timeZone="UTC"
+		>
+			<NextUIProvider>{children}</NextUIProvider>
+		</NextIntlClientProvider>
+	);
 }

957cc35e9093867e6c1d8b770e5210d4de2bf957
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-09 18:09:21 +0400

    Implemented client-side internationalization

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,29 +1,14 @@
-import type { AbstractIntlMessages } from "next-intl";
-import { unstable_setRequestLocale } from "next-intl/server";
-import { useMessages } from "next-intl";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 
-type CustomPageProps = {
-	params: { locale: string };
-};
-
-export default function LocalePage({
-	params: { locale }
-}: Readonly<CustomPageProps>) {
-	unstable_setRequestLocale(locale);
-	const messages: AbstractIntlMessages = useMessages();
-
+export default function LocalePage() {
 	return (
 		<>
 			<header className="absolute top-0 left-0 right-0 z-50">
-				<Header messages={messages.header} />
+				<Header />
 			</header>
 			<main>
-				<Hero
-					heroMessages={messages.hero}
-					featuresMessages={messages.features}
-				/>
+				<Hero />
 			</main>
 			<footer></footer>
 		</>

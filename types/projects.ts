fa9ae4896ef66afe6fe1211d2e3093bde1902250
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-20 21:35:14 +0400

    Enhanced the Project type with Features details

--- types/projects.ts
+++ types/projects.ts
@@ -1,12 +1,18 @@
 import type { ReactNode } from "react";
 import { ReactElement } from "react";
 
+export type FeatureDetails = {
+	title: string;
+	description: string;
+};
+
 export type ProjectDetails = {
 	key: string;
 	name: string;
 	logo?: string;
 	cover?: string;
 	description: string | ReactElement | ReactNode[];
+	features: FeatureDetails[];
 	url?: string;
 };
 

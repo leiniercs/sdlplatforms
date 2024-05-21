a6b8c20033d89e7767b71fadb1815616ff952085
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-21 20:03:52 +0400

    Initial sync

---
+++ components/header/navitem.tsx
@@ -0,0 +1,42 @@
+"use client";
+import type { NavMenuItem } from "@/types/menu";
+import { Link as UILink } from "@nextui-org/react";
+import { Link, usePathname } from "@/components/common/navigation";
+
+type CustomProps = {
+	item: NavMenuItem;
+	isActive: boolean;
+	setIsMenuOpen: (isOpen: boolean) => void;
+	collapsedMenu: boolean;
+};
+
+export default function NavItem({
+	item,
+	isActive,
+	setIsMenuOpen,
+	collapsedMenu
+}: CustomProps) {
+	return (
+		<UILink
+			as={Link}
+			href={item.href.at(0) === "#" ? "#" : item.href}
+			className={collapsedMenu ? "w-full" : undefined}
+			size={collapsedMenu ? "lg" : undefined}
+			color={isActive ? "primary" : "foreground"}
+			aria-current={isActive ? "page" : undefined}
+			onPress={(e) => {
+				setIsMenuOpen(false);
+
+				if (item.href.at(0) === "#") {
+					window.scrollTo({
+						top: document.getElementById(item.href.substring(1))
+							?.offsetTop,
+						behavior: "smooth"
+					});
+				}
+			}}
+		>
+			{item.name}
+		</UILink>
+	);
+}

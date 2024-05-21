bf9c6dd9ba9239b174f31a68801b9eda04cc28de
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-21 21:53:14 +0400

    - Set default font size - Added Contact component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import WhoTrustUs from "@/components/whotrustus";
 import Projects from "@/components/projects";
+import Contact from "@/components/contact";
 
 type CustomPageProps = {
 	params: { locale: string };
@@ -13,10 +14,11 @@ export default function LocalePage({ params: { locale } }: CustomPageProps) {
 			<header className="absolute top-0 left-0 right-0 z-50">
 				<Header />
 			</header>
-			<main>
+			<main className="text-base lg:text-lg">
 				<Hero />
 				<WhoTrustUs />
 				<Projects />
+				<Contact />
 			</main>
 			<footer></footer>
 		</>

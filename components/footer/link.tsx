1d498a9408626e5bac0f34a624368e0821fcf94f
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-23 21:54:58 +0400

    Removed the animated scroll

--- components/footer/link.tsx
+++ components/footer/link.tsx
@@ -7,21 +7,7 @@ type CustomProps = { item: NavMenuItem };
 
 export default function Link({ item }: Readonly<CustomProps>) {
 	return (
-		<UILink
-			as={NavLink}
-			href={item.href.at(0) === "#" ? "#" : item.href}
-			size="lg"
-			color="foreground"
-			onPress={(e) => {
-				if (item.href.at(0) === "#") {
-					window.scrollTo({
-						top: document.getElementById(item.href.substring(1))
-							?.offsetTop,
-						behavior: "smooth"
-					});
-				}
-			}}
-		>
+		<UILink as={NavLink} href={item.href} size="lg" color="foreground">
 			{item.name}
 		</UILink>
 	);

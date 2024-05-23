71e692d7e43d054babeba53b6dd0e36b7e45cd45
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-23 20:00:38 +0400

    Remove the unnecesary <header> HTML tag

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header";
 import Hero from "@/components/hero";
 import WhoTrustUs from "@/components/whotrustus";
 import Projects from "@/components/projects";
@@ -10,17 +9,11 @@ type CustomPageProps = {
 
 export default function LocalePage({ params: { locale } }: CustomPageProps) {
 	return (
-		<>
-			<header className="absolute top-0 left-0 right-0 z-50">
-				<Header />
-			</header>
-			<main className="text-base lg:text-lg">
-				<Hero />
-				<WhoTrustUs />
-				<Projects />
-				<Contact />
-			</main>
-			<footer></footer>
-		</>
+		<main>
+			<Hero />
+			<WhoTrustUs />
+			<Projects />
+			<Contact />
+		</main>
 	);
 }

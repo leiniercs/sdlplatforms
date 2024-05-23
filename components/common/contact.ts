b0829342dd240516e8f3368d7ac07099befe1418
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-23 21:31:59 +0400

    Added social media accounts information

--- components/common/contact.ts
+++ components/common/contact.ts
@@ -1,5 +1,29 @@
-export const contact = {
+import type { ContactDetails } from "@/types/contact";
+import {
+	FaLinkedin,
+	FaXTwitter,
+	FaFacebook,
+	FaInstagram,
+	FaTelegram,
+	FaWhatsapp
+} from "react-icons/fa6";
+
+export const contact: ContactDetails = {
 	address: "Building A1, Dubai Digital Park, Dubai Silicon Oasis",
 	emirate: "Dubai",
-	country: "United Arab Emirates"
+	country: "United Arab Emirates",
+	socialMedia: [
+		{
+			icon: FaLinkedin,
+			url: "https://www.linkedin.com/company/sdlplatforms"
+		},
+		{ icon: FaXTwitter, url: "https://www.x.com/sdlplatforms" },
+		{ icon: FaFacebook, url: "https://www.facebook.com/sdlplatforms" },
+		{ icon: FaInstagram, url: "https://www.instagram.com/sdlplatforms" },
+		{ icon: FaTelegram, url: "https://t.me/sdlplatforms" },
+		{
+			icon: FaWhatsapp,
+			url: "https://chat.whatsapp.com/DGWzyx97KwaC4x6rRvQ59W"
+		}
+	]
 };

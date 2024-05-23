cbcb66d927943b25d3293fe489e74fd56858f22a
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-05-23 21:55:11 +0400

    Removed the animated scroll

--- components/header/navitem.tsx
+++ components/header/navitem.tsx
@@ -19,21 +19,13 @@ export default function NavItem({
 	return (
 		<UILink
 			as={Link}
-			href={item.href.at(0) === "#" ? "#" : item.href}
+			href={item.href}
 			className={collapsedMenu ? "w-full" : undefined}
 			size={collapsedMenu ? "lg" : undefined}
 			color={isActive ? "primary" : "foreground"}
 			aria-current={isActive ? "page" : undefined}
 			onPress={(e) => {
 				setIsMenuOpen(false);
-
-				if (item.href.at(0) === "#") {
-					window.scrollTo({
-						top: document.getElementById(item.href.substring(1))
-							?.offsetTop,
-						behavior: "smooth"
-					});
-				}
 			}}
 		>
 			{item.name}

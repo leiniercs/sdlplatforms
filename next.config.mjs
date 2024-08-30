e9d728597edfb0d113ca9dfc96c9222c3253ff4b
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-08-30 15:56:45 +0400

    Improvements

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,9 @@ const cspHeader = `
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
-    font-src 'self';
+    font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+    frame-src 'self' https://www.google.com;
     object-src 'none';
-    base-uri 'self';
-    form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
 `;
